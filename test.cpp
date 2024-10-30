708fe7cb36a0881e96e4d75a9dd2ae902c86a463
Sullivan Smith <sullisgonerogue@gmail.com>
2024-10-30 09:38:36 -0400

    Initial commit to connect to repo

---
+++ test.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    cout << "Testing testing";
+    for (int i = 0; i<3; i++){
+        cout << " " << i+1;
+    }
+    cout << endl;
+    return 0;
+}
