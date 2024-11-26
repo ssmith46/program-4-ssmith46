3353830611f0c7062042ad305b0043c50cfd3cff
Sullivan Smith <sullisgonerogue@gmail.com>
2024-11-26 14:12:29 -0500

    Command Comments start. Company came over. Will pick up in a bit

--- Command.h
+++ Command.h
@@ -1,3 +1,12 @@
+/*
+Project: Stock Simulator C++ Final Project
+Author: Sullivan Smith
+Date: November 26, 2024
+
+File: Command.h
+Purpose: Contains the declaration of the abstract 'Command' class.
+*/
+
 #ifndef Command_H
 #define Command_H
 
@@ -7,9 +16,31 @@
 #include "Simulator.h"
 
 using namespace std;
+/*
+Purpose:
+- Manipulate the state of the Stock Simulation based on input provided by the user.
+- This is the base class that will be inherited by more specific commands,
+  such as "BuyStockCommand".
 
+Data Members:
+- Simulator s -> The simulator that the command will be able to manipulate.
+- Market m -> The market that the command will be able to manipulate.
+
+- Having access to these two data members will allow the command to change 
+  anything about the Simulation.
+
+Member Functions:
+- Command(string type, vector<string> args, Market m, Simulator s)
+    -> The constructor for the command object. 
+- execute() -> A void abstract method that is overidden by child commands
+
+Class Usage:
+- This class is used to manipulate the simulation based off of
+  user entered strings to specify arguments. 
+*/
 class Command {
     public:
+        /*Constructor*/
         Command(string type, vector<string> args, Market m, Simulator s);
         void execute();
     private:
